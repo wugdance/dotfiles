3a23d557f9ad0022ff3cc6c7125087378a931a1c
wugdance <sintes4ek@gmail.com>
2025-03-30 18:18:04 +0300

    Upd with copying readline library.

--- save.sh
+++ save.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 # Copy from user home directory to repo directory.
 echo "Copying tmux conf..."
 cp ~/.tmux.conf ./.tmux.conf
 
 echo "Copying bash conf..."
 cp ~/.bashrc ./.bashrc
+
+echo "Copying readline conf..."
+cp ~/.inputrc ./.inputrc
 
 echo "The saving process have been completed."
