ea6e501743935c5f3afc6e35ff3922be6124b25e
wugdance <sintes4ek@gmail.com>
2025-03-30 16:00:35 +0300

    Add some output to clarifying current script state.

--- save.sh
+++ save.sh
@@ -1,3 +1,8 @@
 # Copy from user home directory to repo directory.
+echo "Copying tmux conf..."
 cp ~/.tmux.conf ./.tmux.conf
+
+echo "Copying bash conf..."
 cp ~/.bashrc ./.bashrc
+
+echo "The saving process have been completed."
