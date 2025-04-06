65896528477c1d0700cef7536ed302928f0bd9cf
wugdance <sintes4ek@gmail.com>
2025-04-06 23:17:58 +0300

    Add loading glzr config.

--- load.sh
+++ load.sh
@@ -15,4 +15,15 @@ cp ./.bashrc ~/.bashrc
 echo "Loading readline conf..."
 cp ./.inputrc ~/.inputrc
 
+#--------------------------------------------
+echo "Loading the tiling manager config..."
+
+win_user=$(cmd.exe /C "echo %USERNAME%" 2>/dev/null | tr -d '\r')
+glzr="/mnt/c/Users/${win_user}/.glzr"
+
+cp ./.glzr/glazewm/config.yaml ${glzr}/glazewm/config.yaml
+cp ./.glzr/zebar/settings.json ${glzr}/zebar/settings.json
+cp -r ./.glzr/zebar/starter-v2 ${glzr}/zebar/starter-v2/
+#--------------------------------------------
+
 echo "The loading process have been completed."
