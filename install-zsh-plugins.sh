0b42166b889439932127d4fa7c217d449df5966f
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2025-07-25 16:14:47 +0300

    add script for installing zsh plugins

---
+++ install-zsh-plugins.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+set -e
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ~/.zsh/zsh-completions
+git clone https://github.com/jeffreytse/zsh-vi-mode ~/.zsh/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
